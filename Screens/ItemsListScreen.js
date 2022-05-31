10d0e477d15742328092665ed60c56ecc72d01de
Gal Gordon <gal.gordon@gmail.com>
2022-05-31 17:06:55 +0300

    added search

--- Screens/ItemsListScreen.js
+++ Screens/ItemsListScreen.js
@@ -1,34 +1,79 @@
-import React, { useEffect, useState } from 'react';
-import { ScrollView, StyleSheet, Text, SafeAreaView} from 'react-native';
-import { getProducts } from './js/helper';
+import React, { useEffect, useState } from "react";
+import { FlatList, StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { getProducts } from "./js/helper";
+import { SearchBar } from "@rneui/themed";
+
+
 const ItemsListScreen = () => {
   const [products, setProducts] = useState([]);
+  const [search, setSearch] = useState("");
+  const [cancel, setCancel] = useState(false);
   useEffect(() => {
     const getData = async () => {
       const products = await getProducts();
-      console.log('products', products);
+      console.log("products", products);
       setProducts(products);
-    }
-    getData()
-  },[])
-  
-  return (
-    <ScrollView style={styles.container}>
-      {products.map((item, index) => {
-        return (
-          <SafeAreaView>
-            <Text key={item.id}>{item.productName}</Text>
-          </SafeAreaView>
-        )
-      })}
-    </ScrollView>
+    };
+    getData();
+  }, [cancel]);
+
+  useEffect(() => {
+    const filteredProducts = products.filter(
+      (product) =>
+        product.productName.toLowerCase().includes(search.toLowerCase())
+    );
+    setProducts(filteredProducts);
+  },[search])
+  const Item = ({ productName, price }) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{productName}</Text>
+      <Text style={styles.price}>{"R" + price}</Text>
+    </View>
   );
-}
+  const renderItem = ({ item }) => (
+    <Item productName={item.productName} price={item.price} />
+  );
 
-const styles = StyleSheet.create(
-  {
-    container: {}
-  }
-)
+  return (
+    <View style={{flex:1}}>
+    <SafeAreaView style={styles.container}>
+      <SearchBar
+      platform="ios" 
+      placeholder="Type Here..."
+      onChangeText={setSearch}
+      value={search}
+      onCancel={() => setCancel(!cancel)}
+      />
+        <FlatList
+          data={products}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+        />
+      </SafeAreaView>
+    </View>
+  );
+};
 
-export default ItemsListScreen
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+  },
+  item: {
+    padding: 8,
+    margin: 8,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderBottomWidth: 1,
+    borderBottomColor: "#F7F7F7",
+  },
+  title: {
+    fontSize: 16,
+  },
+  price: {
+    fontSize: 14,
+    alignSelf: "flex-end",
+  },
+});
+
+export default ItemsListScreen;
